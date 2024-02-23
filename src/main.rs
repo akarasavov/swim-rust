06e071347983a05c85e51f222fe389ae14e545d0
akarasavov <alekseitk@gmail.com>
2024-02-23 09:04:33 +0000

    implement simple binary tree

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
-use std::sync::atomic::AtomicPtr;
-use std::sync::atomic::Ordering::Relaxed;
+
 
 fn main() {
-    let ptr = &mut 5;
-    let atomic_ptr = AtomicPtr::new(ptr);
-    let i1 = unsafe { *atomic_ptr.load(Relaxed) };
-    let i2 = unsafe { *atomic_ptr.load(Relaxed) };
 
-    println!("{:?}", i1);
-    println!("{:?}", i2);
 }
